21b05cbdc671ccf928b6da37d127e3cc1bbd3aad
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-11 11:55:29 +1100

    Add LocationMap component

---
+++ src/components/LocationMap.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LocationMap = ({ location }) => (
+  <iframe
+    title="locationMap"
+    width="600"
+    height="450"
+    frameBorder="0"
+    style={{ border: '0' }}
+    src={
+      'https://www.google.com/maps/embed/v1/place?key=' +
+      process.env.REACT_APP_GOOGLE_API_KEY +
+      '&q=' +
+      location.latitude +
+      ',' +
+      location.longitude +
+      '&zoom=16'
+    }
+    allowFullScreen
+  />
+)
+
+export default LocationMap
