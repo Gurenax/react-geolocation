e96e442bb797930fafffc5f0a2630193bbc674b2
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-11 11:47:30 +1100

    Add google maps iframe

--- src/App.js
+++ src/App.js
@@ -52,9 +52,19 @@ class App extends Component {
           <div className="mt-3">
           {!!location && !error ? (
             <div>
-              <div>Latitude: {location.latitude}</div>
-              <div>Longitude: {location.longitude}</div>
-              <div>Accuracy: {location.accuracy}</div>
+              <div><strong>Latitude:</strong> {location.latitude}</div>
+              <div><strong>Longitude:</strong> {location.longitude}</div>
+              <div><strong>Accuracy:</strong> {location.accuracy}</div>
+              <div className="mt-3">
+                <iframe
+                  title="locationMap"
+                  width="600"
+                  height="450"
+                  frameBorder="0"
+                  style={{border: '0'}}
+                  src={"https://www.google.com/maps/embed/v1/place?key="+process.env.REACT_APP_GOOGLE_API_KEY+"&q="+location.latitude+","+location.longitude+"&zoom=16"} allowFullScreen>
+                </iframe>
+              </div>
             </div>
           ) : <div>{error}</div>
           }
