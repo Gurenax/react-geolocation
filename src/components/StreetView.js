b61daedc711ddd118c5e2116f75b907f1db0e3e0
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-12 22:20:00 +1100

    Add StreetView component

---
+++ src/components/StreetView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const StreetView = ({ location }) => (
+  <iframe
+    width="600"
+    height="450"
+    frameBorder="0"
+    style={{ border: '0' }}
+    src={
+      'https://www.google.com/maps/embed/v1/streetview?key=' +
+      process.env.REACT_APP_GOOGLE_API_KEY +
+      '&location=' +
+      location.latitude +
+      ',' +
+      location.longitude +
+      '&heading=180&pitch=0&fov=90'
+    }
+    allowFullScreen
+  />
+)
+
+export default StreetView
